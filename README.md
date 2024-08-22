e797b2519fb373f30e84c3141710ee4b3d48449a
Harm1990 <84412382+Harm1990@users.noreply.github.com>
2024-08-22 15:20:23 +0200

    Corrected behaviour with missing lines
    Script now has improved handling in case lines are faulty and skipped. The line numbering is leading in showing the verbose output.

--- README.md
+++ README.md
@@ -2,7 +2,19 @@
  Decode Demcon Challenge - festival schedule generator
 
 ## How to use
+_Optional:   Generate a random list of shows using "exampleGenerator.py". Run this script from your favorite Python editor 
+or in PowerShell using "python exampleGenerator.py". Answer the three questions in the terminal. This will generate a
+file called randomLineup.txt that can be used as input for main.py_
 
+To run the script:
+1) In main.py, adjust the required variables to suit your needs
+   1) In line 13, adjust inputFile to indicate which file contains the show list 
+   2) Set the flags verboseOutput and imageOutput (line 16 and 17) to True or False depending on what kind of output you wish
+2) Run the script (e.g. using your favorite editor or use "python main.py" in PowerShell)
+3) Appreciate the output
+   1) If verboseOutput was True, the output is given in the terminal in the form of a table separated by tabs. 
+The top row indicates the festival hours, the following rows indicate which show (in the form of line number) are at which stage
+   2) If imageOutput was True, "planningProgram.png" will be generated with a column per stage
 
 ## Development environment
 Project developed with PyCharm 2024.2 and Python 3.12.5
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Script to generate festival-schedule from input
+# Script to generate festival-schedule from list of shows
 
 # Reads the input from a predefined file in the form of
 # show_name hour_start hour_stop [space separated]
@@ -14,91 +14,100 @@ inputFile = "randomLineup.txt"
 
 # Output
 verboseOutput = True
-imageOutput = False
+imageOutput = True
 
 # Function Variables
 # Create class for shows
 class Show:
-    def __init__(self, name, start, stop):
+    def __init__(self, name, start, stop, line_number):
         self.name = name
         self.start = int(start) # Starting hour
         self.stop = int(stop) # Stopping hour (excluding), e.g. if stop = 10, it stops at 9:59
+        self.line_number = int(line_number)
 
 # Use check occupation to see where show might fit and plan show at closest existing fit
-def planShow(localStagePlan, showNumber, showStart, showStop):
-    showDuration = showStop - showStart
+def plan_show(local_stage_plan, show_number, show_start, show_stop):
+    show_duration = show_stop - show_start
     # If stagePlan is too short, increase length of stagePlan
-    if showStop > len(localStagePlan[0]):
-        for stage in localStagePlan:
-            targetExtension = showStop - len(stage)
-            stage += [0] * targetExtension
+    if show_stop > len(local_stage_plan[0]):
+        additional_show_length = show_stop - len(local_stage_plan[0])
+        for stage in local_stage_plan:
+            stage += [0] * additional_show_length
 
     # Now check if stage is occupied and if not, check closest show
-    stageClearance = []
-    for stage in localStagePlan:
-        if sum(stage[showStart:showStop]) == 0:
+    stage_clearance = []
+    for stage in local_stage_plan:
+        if sum(stage[show_start:show_stop]) == 0:
             # No occupation, check for closest
-            ii = showStart
+            ii = show_start
             while stage[ii] == 0 and ii > 0:
                 ii -= 1 # Descend into list
-            stageClearance.append(showStart - ii)
+            stage_clearance.append(show_start - ii)
         else:
-            stageClearance.append(-1) # Indicate no clearance
+            stage_clearance.append(-1) # Indicate no clearance
 
     # If no stages available, create new stage
-    if sum(stageClearance) == (-1 * len(stageClearance)):
-        localStagePlan.append([0] * len(localStagePlan[0])) # Create new stage
-        localStagePlan[-1][showStart:showStop] = [showNumber] * showDuration # Plan stage
+    if sum(stage_clearance) == (-1 * len(stage_clearance)):
+        local_stage_plan.append([0] * len(local_stage_plan[0])) # Create new stage
+        local_stage_plan[-1][show_start:show_stop] = [show_number] * show_duration # Plan stage
     else:
-        # Plan the show in the non-occupied, stage with least clearance
-        local_minimum = max(stageClearance)+1
-        best_stage = 0 # By default use first stage in case all are equal
-        for ii, clearance in enumerate(stageClearance):
+        # Plan the show in the non-occupied, stage with the least clearance
+        local_minimum = max(stage_clearance)+1
+        best_stage = 0 # By default, use first stage in case all are equal
+        for ii, clearance in enumerate(stage_clearance):
             if -1 < clearance < local_minimum:
                 local_minimum = clearance
                 best_stage = ii
-        localStagePlan[best_stage][showStart:showStop] = [showNumber] * showDuration  # Plan stage
+        local_stage_plan[best_stage][show_start:show_stop] = [show_number] * show_duration  # Plan stage
 
-    return localStagePlan
+    return local_stage_plan
 
 
 def main():
     # Create list that contains all found shows in input file
     # Ensure that number 0 is not linked to a show, to allow 0 to indicate no show is given on that stage, see Stage.occupationHours
-    listShows = [Show('noShow', 0, 0)]
+    list_shows = [Show('noShow', 0, 0, 0)]
 
     # Initial idea was to also use a Stage Class, but since all Stages should have the same event length,
     # decided to go for a nested loop
-    stagePlan = [[0]]  # Start with only 1 hour, nothing planned
+    stage_plan = [[0]]  # Start with only 1 hour, nothing planned
 
 
     # Read all shows into list
     with open(inputFile, 'r') as f:
         lines = f.readlines()
-        for line in lines:
+        for line_number, line in enumerate(lines, start=1):
             try:
-                name, start, stop = line.split()
+                name, start, stop = line.split() # Extract values from line
                 if int(start) > int(stop):
                     raise ValueError
                 # The description indicates the stop is included, whilst the script is based on stop being excluded
                 stop = int(stop) + 1
-                listShows.append(Show(name,start,stop))
+                list_shows.append(Show(name,start,stop,line_number))
             except ValueError:
-                print("Error reading line: %s" % line.replace("\r\n", "").replace("\n", ""))
-                print("Skipped over line and continued")
+                print("Error reading line %i: %s" %(line_number,line.replace("\r\n", "").replace("\n", "")) )
+                print("Line is excluded, but script will process rest of file")
 
-    # For every show, plan show in Stage plan
-    for showInt, show in enumerate(listShows):
-        if showInt != 0: # Skip show 0
-            stagePlan = planShow(stagePlan,showInt,show.start,show.stop)
+    # For every show, plan show into Stage plan
+    for show in list_shows:
+        if show.line_number != 0: # Skip show 0
+            stage_plan = plan_show(stage_plan,show.line_number,show.start,show.stop)
 
     # Now create output
     if verboseOutput:
-        print("Hours: \t\t" + "\t|".join(map(str, range(len(stagePlan[0])) )))
-        for stageInt, stage in enumerate(stagePlan):
-            print("Stage %i: \t" %stageInt + "\t|".join(map(str, stage )))
+        if list_shows[-1].line_number > 99:
+            # Do not use separating '|', since it screws up formatting
+            print("Hours: \t\t" + "\t".join(map(str, range(len(stage_plan[0])))))
+            for stageInt, stage in enumerate(stage_plan):
+                print("Stage %i: \t" % (stageInt+1) + "\t".join(map(str, stage)))
+        else:
+            print("Hours: \t\t" + "\t|".join(map(str, range(len(stage_plan[0])))))
+            for stageInt, stage in enumerate(stage_plan):
+                print("Stage %i: \t" % (stageInt+1) + "\t|".join(map(str, stage)))
 
-
+    if imageOutput:
+        # Move to exterior function
+        pass
 
 
 
